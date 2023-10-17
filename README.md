776a6820d39a3e006add9325e75660ec8ef5368d
Pablo Moreno <pablo_moreno_martin@hotmail.com>
2023-10-17 20:16:27 +0200

    Full commit
    In this commit the full webpage is uploaded, as it was worked locally until it was finished.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# banco_concept
- 
+# Concepto de Landing Page para un banco
+
+### [Página funcional](https://morenno.net/demoWebs/Banco/)
+
+![Landing page de local de comidas para llevar](https://morenno.net/demoWebs/img_thumbnails/Banco.png)
+
+## Contenidos
+
+- Componentes funcionales y reutilizables de React
+- Esctructura de archivos
+- Uso de Tailwind para los estilos CSS
+- Creación de hojas de estilos propias con aquellos reutilizables (fuentes, degradados...)
+- Uso de Vite para el desarrollo en local
+- Uso de animaciones básicas
+- Uso de "Breakpoint prefixes" de Tailwind para hacer una web apta para todos los dispositivos (responsive)
---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[480px] mt-5`}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum sequi
+        eveniet sunt qui exercitationem, officiis quia.
+      </p>
+      <Button styles="mt-10" />
+    </div>
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className="w-[100%] h-[100%] object-contain" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
