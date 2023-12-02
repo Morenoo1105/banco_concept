1a0ef37fcaadc0377eacb77e01b4ed7f63ddec39
Pablo Moreno <pablo_moreno_martin@hotmail.com>
2023-12-02 13:46:29 +0100

    Updated Favicon and Route

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://morenno.net/demoWebs/Banco",
+  base: "https://app.morenno.net/hoobank",
 });
